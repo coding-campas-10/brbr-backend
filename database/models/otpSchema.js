b6bc102e46720550985ddaaece10eeec1153f668
electrochemistry04 <electrochemistry04@outlook.com>
2021-06-04 07:28:29 +0900

    fix: key "createdAt"을 "created_at"으로 수정

--- database/models/otpSchema.js
+++ database/models/otpSchema.js
@@ -6,8 +6,7 @@ dotenv.config('../');
 const otpSchema = mongoose.model('barcode-otps', new mongoose.Schema({
     user_id: { type: Number, required: true, unique: true },
     otp_code: { type: String, required: true },
-    createdAt: { type: Date, required: true, expires: parseInt(process.env.OTP_EXPIRE), default: Date.now}
-    })
-);
+    created_at: { type: Date, required: true, expires: parseInt(process.env.OTP_EXPIRE), default: Date.now}
+}));
 
 export default otpSchema;
