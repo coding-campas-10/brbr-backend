f19e2144784ef996856821bf33880df329c9f70a
electrochemistry04 <electrochemistry04@outlook.com>
2021-06-04 08:07:53 +0900

    add: kakao login 검증 절차 추가

--- router/auth.js
+++ router/auth.js
@@ -8,29 +8,48 @@ const router = express.Router();
 
 dotenv.config('../');
 
+const verifyKakaoToken = async (accessToken) => {
+    try{
+        const user = await axios({
+            method:'get',
+            url:'https://kapi.kakao.com/v2/user/me',
+            headers:{
+                Authorization: `Bearer ${accessToken}`
+            }//헤더에 내용을 보고 보내주겠다.
+        })
+
+        return user
+    }
+    catch{
+        throw new Error('카카오 계정 정보를 확인할 수 없습니다')
+    }
+};
+
 const login = async (req, res) => {   //우리측 DB와 대조하기만 하면 됨, 회원가입 안되어있으면 401 반환
     try{
-        const exUser = await userDB.findOne({user_id: req.body.user_id});
-        if(!exUser) { res.status(401).send('등록되지 않은 카카오 계정입니다') };
+        const user = verifyKakaoToken(req.body.access_token);
+        const exUser = await userDB.findOne({user_id: user.id});
+        if(!exUser) { throw new Error('등록되지 않은 카카오 계정입니다') };
         
         req.session.user_id = req.body.user_id;
         res.status(200).send('로그인 성공');
     }
     catch(e){
-        res.status(400).send();
+        res.status(400).send(e);
     }
 }
 
 /*
 {
     "access_token": "token",
-    ""
+    ...
 }
 */
 
 const register = async (req, res) => {  //회원가입 되어있으면 401 반환, 이외 에러는 400 반환, 아니면 userDB와 walletDB 생성하고 200 반환
     try{
-        const exUser = await userDB.findOne({user_id: req.body.id});    //kakao측 user 데이터쪽에 맞춰서 user_id 대신 id 사용
+        const user = verifyKakaoToken(req.body.access_token);
+        const exUser = await userDB.findOne({user_id: user.id});    //kakao측 user 데이터쪽에 맞춰서 user_id 대신 id 사용
         if(exUser) { res.status(401).send('이미 등록된 카카오 계정입니다.') };
         
         const registerUser = new userDB({
