17e37b6572b078b01fd6b0ffad2c3b36a95ad910
electrochemistry04 <electrochemistry04@outlook.com>
2021-06-05 20:03:18 +0900

    add: point save logic

--- router/wallets.js
+++ router/wallets.js
@@ -3,14 +3,20 @@ import walletDB from '../database/models/walletSchema.js';
 
 const router = express.Router();
 
+const getPoint = (body) => {
+
+    let point = body.weights.plastic * 10 + body.weights.paper * 10 + body.weights.glass * 10 + body.weights.styrofoam * 10 + body.weights.etc * 10;
+
+    return point;
+}
+
 const makeReceipt = async (req, res) => { //station을 특정할 수 있는 key 필요
     try{
         const wallet = await walletDB.findOne({ user_id: req.session.user_id });
         const point = getPoint(req.body);
         wallet.receipts.push({
             station_id: req.body.station_id,
-            point: req.body.point,    //backend에서 weight 기반으로 포인트 정산하는 모듈 필요
-                            //그리고 그걸 wallet에 합산해야함
+            point: point,
 
             plastic_weight: req.body.weights.plastic,
             paper_weight: req.body.weights.paper,
@@ -18,9 +24,9 @@ const makeReceipt = async (req, res) => { //station을 특정할 수 있는 key 
             styrofoam_weight: req.body.weights.styrofoam,
             etc_weight: req.body.weights.etc,
         })
-        // await walletDB.updateOne({ user_id: req.session.user_id }, {
-        //     total_points: wallet.total_points + 
-        // });
+        await walletDB.updateOne({ user_id: req.session.user_id }, {
+            total_points: wallet.total_points + point
+        });
         await wallet.save()
         res.status(200).send('거래 성공');
     }
