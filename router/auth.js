509dd0504443f0db486e511a4e09b9c1de21547c
electrochemistry04 <electrochemistry04@outlook.com>
2021-06-12 11:05:46 +0900

    add: leave service

--- router/auth.js
+++ router/auth.js
@@ -10,12 +10,16 @@ dotenv.config('../');
 
 const login = async (req, res) => {   //우리측 DB와 대조하기만 하면 됨, 회원가입 안되어있으면 401 반환
     try{
-        const exUser = await userDB.findOne({user_id: req.body.user_id});
-        if(!exUser) { return res.status(401).send('등록되지 않은 카카오 계정입니다'); };
-        
-        req.session.user_id = req.body.user_id;
-        logger.info(`user ${req.body.user_id} login at ${new Date}`);
-        res.status(200).json(exUser);
+        const exUser = await userDB.findOne({user_id: req.body.user_id}, {_id:0, user_id:1, name:1, connected_at:1});
+        if(!exUser) { 
+            req.session.destroy();
+            return res.status(401).send('등록되지 않은 카카오 계정입니다'); 
+        }
+        else{
+            req.session.user_id = req.body.user_id;
+            logger.info(`user ${req.body.user_id} login`);
+            res.status(200).json(exUser);
+        }
     }
     catch(e){
         logger.error(e);
@@ -25,8 +29,11 @@ const login = async (req, res) => {   //우리측 DB와 대조하기만 하면 �
 
 const register = async (req, res) => {  //회원가입 되어있으면 401 반환, 이외 에러는 400 반환, 아니면 userDB와 walletDB 생성하고 200 반환
     try{
-        const exUser = await userDB.findOne({user_id: req.body.user_id});    //kakao측 user 데이터쪽에 맞춰서 user_id 대신 id 사용
-        if(exUser) { return res.status(401).send('이미 등록된 카카오 계정입니다.'); };
+        const exUser = await userDB.findOne({user_id: req.body.user_id}, {_id:0, user_id:1, name:1, connected_at:1});    //kakao측 user 데이터쪽에 맞춰서 user_id 대신 id 사용
+        if(exUser) { 
+            req.session.destroy();
+            return res.status(401).send('이미 등록된 카카오 계정입니다.'); 
+        };
         
         const registerUser = new userDB({
             user_id: req.body.user_id,
@@ -51,12 +58,14 @@ const register = async (req, res) => {  //회원가입 되어있으면 401 반�
 }
 
 const logout = (req, res) => {  //우리측 세션만 만료
+    logger.info(`user ${req.session.user_id} logout`);
     req.session.destroy();
     res.status(204).send('로그아웃 성공');
 }
 
 const getUserInfo = async (req, res) => {
     try{
+        console.log(req.session);
         const exUser = await userDB.findOne({user_id: req.session.user_id}, {_id:0, user_id:1, name:1, connected_at:1});
         if(!exUser) { return res.status(401).send('계정 정보를 찾을 수 없습니다.')};
         res.status(200).send(exUser);
@@ -67,6 +76,20 @@ const getUserInfo = async (req, res) => {
     }
 }
 
+const leave = async (req, res) => {
+    try{
+        await userDB.deleteOne({user_id: req.session.user_id});
+        await walletDB.deleteOne({user_id: req.session.user_id});
+        req.session.destroy();
+
+        return res.status(204).send();
+    }
+    catch(e){
+        return res.status(400).send();
+    }
+}
+
+router.delete('/leave', leave);
 router.post('/login', login);
 router.post('/register', register);
 router.get('/logout', logout );
