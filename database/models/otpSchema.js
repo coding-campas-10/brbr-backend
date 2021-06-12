52e6369543edbd3d5b3cbe82436249e7e9a9a340
electrochemistry04 <electrochemistry04@outlook.com>
2021-06-12 14:21:54 +0900

    fix: OTP 중복 발급 불가 문제 해결

--- database/models/otpSchema.js
+++ database/models/otpSchema.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config('../');
 
 const otpSchema = mongoose.model('barcode-otps', new mongoose.Schema({
-    user_id: { type: Number, required: true, unique: true },
+    user_id: { type: Number, required: true },
     otp_code: { type: String, required: true },
     created_at: { type: Date, required: true, expires: parseInt(process.env.OTP_EXPIRE), default: Date.now}
 }));
